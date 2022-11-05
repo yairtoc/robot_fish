cd912f3d96dbdc21c98e748f2c8d87d4cd6ecba7
yairtoc <yair.toc@gmail.com>
2022-11-05 10:00:00 +0200

    refactor and cleanup

--- fish_wire_len.py
+++ fish_wire_len.py
@@ -5,27 +5,31 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-rot = lambda theta: np.array([[np.cos(-theta),-np.sin(-theta)], [np.sin(-theta), np.cos(-theta)]])
+#constants
 h = 1
 r_base = 2
 r = 1
 offset = 1
+theta = np.pi/8
 
+# vertices for drawing
 base = [[0,h],[0,0],[-r_base,0], [r_base,0]]
+top = [[0,0],[0,h],[-r,h], [r,h]]
 
-verts = [[0,0],[0,h],[-r,h], [r,h]]
+# calculations
+rot = lambda theta: np.array([[np.cos(-theta),-np.sin(-theta)], [np.sin(-theta), np.cos(-theta)]])
+rot_top = lambda t: np.dot(rot(t), np.array(top).T).T + np.array([0,h])
+l = lambda t: np.linalg.norm(rot_top(t)[2:4]-base[2:4],axis=1)
 
-rot_verts = lambda t: np.dot(rot(t), np.array(verts).T).T + np.array([0,h])
-
-l = lambda t: np.linalg.norm(rot_verts(t)[2:4]-base[2:4],axis=1)
-
-for p in base, rot_verts(0), rot_verts(np.pi/8):
+# drawing
+plt.figure()
+for p in base, rot_top(0), rot_top(theta):
     x, y = zip(*p)
     plt.plot(x, y)
 plt.axis("equal")
 plt.grid()
 plt.show()
-
-
-print(f"delta_l = {l(np.pi/8)-l(0)}")
+print(f"l_init =  {l(0)}")
+print(f"l_rot =   {l(theta)}")
+print(f"delta_l = {l(theta)-l(0)}")
 
